d52c5f87915bfb536838be0778cfa40b12bb6fe6
firestar2009 <alexander.kilbride@gmail.com>
2025-02-22 14:14:10 -0500

    added template for integrating vision processing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,8 +12,14 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -22,7 +28,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends TimedRobot {
 
-
+  Thread m_visionThread;
     
   
   private Command m_autonomousCommand;
@@ -42,6 +48,45 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    m_visionThread =
+    new Thread(
+        () -> {
+          // Get the UsbCamera from CameraServer
+          UsbCamera camera = CameraServer.startAutomaticCapture();
+          // Set the resolution
+          camera.setResolution(640, 480);
+
+          // Get a CvSink. This will capture Mats from the camera
+          CvSink cvSink = CameraServer.getVideo();
+          // Setup a CvSource. This will send images back to the Dashboard
+          CvSource outputStream = CameraServer.putVideo("Rectangle", 640, 480);
+
+          // Mats are very memory expensive. Lets reuse this Mat.
+          Mat mat = new Mat();
+
+          // This cannot be 'true'. The program will never exit if it is. This
+          // lets the robot stop this thread when restarting robot code or
+          // deploying.
+          while (!Thread.interrupted()) {
+            // Tell the CvSink to grab a frame from the camera and put it
+            // in the source mat.  If there is an error notify the output.
+            if (cvSink.grabFrame(mat) == 0) {
+              // Send the output the error.
+              outputStream.notifyError(cvSink.getError());
+              // skip the rest of the current iteration
+              continue;
+            }
+            // Put a rectangle on the image (template processing)
+            Imgproc.rectangle(
+                mat, new Point(100, 100), new Point(400, 400), new Scalar(255, 255, 255), 5);
+            // Give the output stream a new image to display
+            outputStream.putFrame(mat);
+          }
+        });
+m_visionThread.setDaemon(true);
+m_visionThread.start();
+
         autoChooser = new AutoChooser();
 
         // Add options to the chooser
@@ -51,7 +96,7 @@ public class Robot extends TimedRobot {
         autoChooser.addRoutine("MAlgae to RSource", m_robotContainer::MAlgaetoRSource);
         autoChooser.addRoutine("RSource to Reef4", m_robotContainer::RSourcetoReef4Routine);
         autoChooser.addRoutine("ReefFullLoop", m_robotContainer::ReefFullLoopRoutine);
-        autoChooser.addRoutine("forwardrighttop", m_robotContainer::forwardrighttop);
+       // autoChooser.addRoutine("forwardrighttop", m_robotContainer::forwardrighttop);
 
 
 
@@ -144,6 +189,8 @@ public class Robot extends TimedRobot {
   @Override
   public void testPeriodic() {}
 
+
+  
 
 
   
